edd4d85a39da77f6484047c66744e9edc418b47f
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-11-15 03:07:05 +0100

    Recode 😄

---
+++ src/fm.rs
@@ -0,0 +1,89 @@
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
+use std::path::{PathBuf};
+use rfd::FileDialog;
+
+pub struct FM
+{
+    path: Option<PathBuf>,
+    save_path: Option<PathBuf>,
+    file_name: Option<String>,
+    buffer: Vec<u8>,
+}
+
+impl FM
+{
+    pub fn new() -> Self
+    {
+        Self { path: None,
+               save_path: None,
+               file_name: None,
+               buffer: Vec::new() }
+    }
+
+    pub fn load_file(&mut self)
+    {
+        self.select_file();
+        let file = File::open(self.path.as_ref()
+            .expect("Failed to open file")).expect("File not found");
+        let mut reader = BufReader::new(file);
+        reader.read_to_end(&mut self.buffer).expect("Can't read buffer");
+    }
+
+    pub fn create_c_file(&mut self) -> std::io::Result<()>
+    {
+        if self.buffer.is_empty() {
+            println!("Buffer is empty. Load a file first.");
+            return Ok(());
+        }
+
+        if self.file_name.is_none() {
+            panic!("File name not set. Load a file first.");
+        }
+
+        let file_name = self.file_name.as_ref().unwrap();
+        self.save_path = FileDialog::new()
+            .set_file_name(&format!("{}.c", file_name))
+            .add_filter("C sources (*.c)", &["c"])
+            .save_file();
+
+        let mut c_file = File::create(self.save_path.as_ref().expect("No save path selected"))?;
+        let declaration = format!("unsigned char rawData[{}] = {{\n", self.buffer.len());
+        c_file.write_all(declaration.as_bytes())?;
+
+        let mut counter:u8 = 1;
+        for &value in &self.buffer
+        {
+            if counter == 1 {
+                c_file.write_all(b"    ")?;
+            }
+
+            let str: String = format!("0x{:02X}, ", value);
+            c_file.write_all(str.as_bytes())?;
+
+            if counter == 12 {
+                c_file.write_all(b"\n")?;
+                counter = 1;
+            } else {
+                counter += 1;
+            }
+        }
+
+        c_file.write_all(b"\n};")?;
+        Ok(())
+    }
+
+    fn select_file(&mut self)
+    {
+        if let Some(selected_path) = FileDialog::new().pick_file()
+        {
+            self.path = Some(selected_path.clone());
+            self.file_name = selected_path.file_stem()
+                .and_then(|stem| stem.to_str())
+                .map(|stem| stem.to_string());
+        }
+        else {
+            panic!("No file selected");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,12 @@
-use std::fs::File;
-use std::io::{BufReader, Read, Write};
 use std::process::Command;
-use rfd::FileDialog;
 use device_query::{DeviceQuery, DeviceState, Keycode};
 
-fn get_path() -> File
-{
-    let path = FileDialog::new()
-        .pick_file()
-        .expect("No file selected");
-
-    let file = File::open(path).expect("Failed to open file");
-
-    file
-}
-
-fn get_file_buffer(file: File) -> Vec<u8>
-{
-    let mut reader: BufReader<File> = BufReader::new(file);
-    let mut buffer: Vec<u8> = Vec::new();
-    reader.read_to_end(&mut buffer).expect("Can't read buffer");
-
-    buffer
-}
-
-fn create_c_file(buffer: &Vec<u8>) -> std::io::Result<()>
-{
-    let save_path = FileDialog::new()
-        .set_file_name("test.c")
-        .add_filter("C sources (*.c)", &["c"])
-        .save_file();
-
-    let mut c_file: File = File::create(save_path.unwrap())?;
-
-    let declaration: String = format!("unsigned char rawData[{}] = {{\n", buffer.len());
-    c_file.write_all(declaration.as_bytes())?;
-
-    let mut counter: i32 = 1;
-    for value in buffer
-    {
-        if counter == 12 {
-            c_file.write_all(b"\n")?;
-            counter = 1;
-        }
-
-        if counter == 1 {
-            c_file.write_all(b"    ")?;
-        }
-
-        let str = if *value <= 0xF {
-            format!("0x0{:X}, ", value)
-        } else {
-            format!("0x{:X}, ", value)
-        };
-        c_file.write_all(str.as_bytes())?;
-
-        counter += 1;
-    }
-
-    c_file.write_all(b"\n};")?;
-
-    Ok(())
-}
+mod fm;
+use fm::FM;
 
 fn main()
 {
-    let mut buffer: Vec<u8> = Vec::new();
+    let mut file_manager = FM::new();
     let device_state: DeviceState = DeviceState::new();
 
     println!("[0] Exit");
@@ -80,27 +21,20 @@ fn main()
             break;
         }
 
-        if keys.contains(&Keycode::Key1)
-        {
-            let path: File = get_path();
-            buffer = get_file_buffer(path);
+        if keys.contains(&Keycode::Key1) {
+            file_manager.load_file();
         }
 
         if keys.contains(&Keycode::Key2)
         {
-            if buffer.is_empty() {
-                println!("You need to select a file");
-                break;
-            }
-
-            create_c_file(&buffer)
-                .expect("Can't create C file");
-
+            file_manager.create_c_file().expect("Can't create C file");
             println!("Successfully created a C file");
             break;
         }
     }
 
-    Command::new("cmd").args(&["/C", "pause"]).status()
+    Command::new("cmd")
+        .args(&["/C", "pause"])
+        .status()
         .expect("Error on executing pause");
 }
